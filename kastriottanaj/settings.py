5226bdb0f522e9a7e3c6b6cdb94fdaf3a719ef42
Kastriot Tanaj <kastriot.sym@gmail.com>
2025-11-15 22:52:47 +0100

    Prepare project for deployment to Render (DEBUG=False, env vars, database, static)

--- kastriottanaj/settings.py
+++ kastriottanaj/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oovg$3!)6-a$7t47q0_s4m)_t*o0nwzeu!x*iyoy&i8ax=h@)z'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-dev-fallback')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kastriottanaj.onrender.com', '127.0.0.1']
 
 # Email backend (for dev, use console backend to test)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
